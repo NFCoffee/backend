867b0ada6255b01b7d464704f936736f7ae3a9e6
jang-namu <ironhee8005@gmail.com>
2024-05-12 21:52:22 +0900

    init: dependency

---
+++ src/main/java/org/inu/nfcoffee/handler/StreamLambdaHandler.java
@@ -0,0 +1,20 @@
+package org.inu.nfcoffee.handler;
+
+public class StreamLambdaHandler implements RequestStreamHandler {
+    public static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+        } catch (ContainerInitializationException e) {
+            // if we fail here. We re-throw the exception to force another cold start
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
